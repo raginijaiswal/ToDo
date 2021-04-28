8fa767a06052aa12dd78fa58e76997228e7ad548
raginijaiswal <74948054+raginijaiswal@users.noreply.github.com>
2021-04-28 13:28:20 +0530

    Doc: Updation

--- script1.js
+++ script1.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var button = document.getElementById("enter");
 var input = document.getElementById("username");
 var ul = document.querySelector("ul");
@@ -30,37 +29,4 @@ button.addEventListener("click", addListAfterClick);
 
 
 
-=======
-var button = document.getElementById("enter");
-var input = document.getElementById("username");
-var ul = document.querySelector("ul");
-
-function inputLength() {
-    return input.value.length;
-}
-
-function createListElement(){
-    var li= document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value="";
-}
-
-function addListAfterClick(){
-    if(inputLength() > 0) {
-        createListElement()
-    }
-}
-
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
-}
-
-button.addEventListener("click", addListAfterClick);
-
-
-
->>>>>>> 33918167791340424e6fb5bab60447a8070751e3
 input.addEventListener("keypress", addListAfterKeypress);
